faf12a98b25630d17185da093bd504a2e69261c0
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-04-19 21:59:17 -0700

    Initial Commit

---
+++ lynda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lynda
+//
+//  Created by Koji on 2017/04/19.
+//  Copyright © 2017年 KojiNakagawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
